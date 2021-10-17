183aa4bfa0775b1e3785214385de7307bcab2497
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-10-17 16:34:00 -0700

    Made default manager account be an ApplicationUser

--- BankSite/Models/IdentityHelper.cs
+++ BankSite/Models/IdentityHelper.cs
@@ -28,17 +28,19 @@ namespace BankSite.Models
 
         public static async Task CreateDefaultUserAsync(IServiceProvider provider, string role)
         {
-            var userManager = provider.GetService<UserManager<IdentityUser>>();
+            var userManager = provider.GetService<UserManager<ApplicationUser>>();
 
             // if no users are in specified role, make default user in that role
             int numUsers = (await userManager.GetUsersInRoleAsync(role)).Count;
             if (numUsers == 0)
             {
-                var defaultUser = new IdentityUser()
+                var defaultUser = new ApplicationUser()
                 {
                     Email = "manager@bank.com",
                     UserName = "Admin",
-                    EmailConfirmed = true
+                    EmailConfirmed = true,
+                    FirstName = "John",
+                    LastName = "Doe"
                 };
 
                 await userManager.CreateAsync(defaultUser, "Password123#");
