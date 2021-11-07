13886710abc87aa546b83c1cf6b38473fdf5d6e8
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-11-07 00:53:03 -0700

    Made newly registered accounts have the AccountHolder role

--- BankSite/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ BankSite/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -99,6 +99,9 @@ namespace BankSite.Areas.Identity.Pages.Account
                 {
                     _logger.LogInformation("User created a new account with password.");
 
+                    // Add newly registered accounts to AccountHolder role
+                    await _userManager.AddToRoleAsync(user, IdentityHelper.AccountHolder);
+
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
                     var callbackUrl = Url.Page(
