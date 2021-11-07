5b277fc49fee4699ac10f603e962376fdb51dde3
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-11-06 21:42:27 -0700

    Added database migration

---
+++ BankSite/Data/Migrations/20211107044014_AddBalanceColumn.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankSite.Data.Migrations
+{
+    public partial class AddBalanceColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AccountTypes_Accounts_AccountId",
+                table: "AccountTypes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AccountTypes_AccountId",
+                table: "AccountTypes");
+
+            migrationBuilder.DropColumn(
+                name: "AccountId",
+                table: "AccountTypes");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "varchar(75)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "varchar(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TypeName",
+                table: "AccountTypes",
+                type: "varchar(30)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountTypeId",
+                table: "Accounts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "money",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_AccountTypeId",
+                table: "Accounts",
+                column: "AccountTypeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_AccountTypes_AccountTypeId",
+                table: "Accounts",
+                column: "AccountTypeId",
+                principalTable: "AccountTypes",
+                principalColumn: "AccountTypeId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_AccountTypes_AccountTypeId",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_AccountTypeId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "TypeName",
+                table: "AccountTypes");
+
+            migrationBuilder.DropColumn(
+                name: "AccountTypeId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "Balance",
+                table: "Accounts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "LastName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(75)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FirstName",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(50)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AccountId",
+                table: "AccountTypes",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AccountTypes_AccountId",
+                table: "AccountTypes",
+                column: "AccountId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AccountTypes_Accounts_AccountId",
+                table: "AccountTypes",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "AccountId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
