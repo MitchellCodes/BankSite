73168e7b554dda8e18bb70990fd808661f7b215d
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-11-19 21:44:56 -0800

    Updated database to set default account balance

---
+++ BankSite/Data/Migrations/20211120054244_AddedDefaultBalance.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BankSite.Data.Migrations
+{
+    public partial class AddedDefaultBalance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "money",
+                nullable: false,
+                defaultValue: 0m,
+                oldClrType: typeof(decimal),
+                oldType: "money");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Balance",
+                table: "Accounts",
+                type: "money",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "money",
+                oldDefaultValue: 0m);
+        }
+    }
+}
