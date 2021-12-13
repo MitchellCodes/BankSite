f030f3a60fb87ed986804c2e0e888186bb79320a
MitchellCodes <82383787+MitchellCodes@users.noreply.github.com>
2021-12-12 16:17:49 -0800

    Added database migration for cascade deleting

---
+++ BankSite/Data/Migrations/20211213001413_CascadeDeleteAccounts.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BankSite.Data.Migrations
+{
+    public partial class CascadeDeleteAccounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_AspNetUsers_UserId",
+                table: "Accounts");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ApplicationUserId",
+                table: "Accounts",
+                type: "nvarchar(450)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Accounts_ApplicationUserId",
+                table: "Accounts",
+                column: "ApplicationUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_AspNetUsers_ApplicationUserId",
+                table: "Accounts",
+                column: "ApplicationUserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_AspNetUsers_UserId",
+                table: "Accounts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_AspNetUsers_ApplicationUserId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_AspNetUsers_UserId",
+                table: "Accounts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Accounts_ApplicationUserId",
+                table: "Accounts");
+
+            migrationBuilder.DropColumn(
+                name: "ApplicationUserId",
+                table: "Accounts");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_AspNetUsers_UserId",
+                table: "Accounts",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+    }
+}
